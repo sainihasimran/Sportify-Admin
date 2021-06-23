9a6012556e1699c9ac367527fdfda457bfc88bc8
Hemit Khunt <65456586+Hemit-khunt@users.noreply.github.com>
2021-06-23 18:50:25 -0400

    login from fragment using firebase authentication
    connect with firebase

--- app/src/main/java/com/cegep/sportify_admin/LoginFragment.java
+++ app/src/main/java/com/cegep/sportify_admin/LoginFragment.java
@@ -1,64 +1,126 @@
 package com.cegep.sportify_admin;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 public class LoginFragment extends Fragment {
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText admin_email, admin_pass;
+    Button signin_btn;
+    TextView joinus;
+    NavController navController;
 
     public LoginFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        admin_email = view.findViewById(R.id.email_et);
+        admin_pass = view.findViewById(R.id.password_et);
+        signin_btn = view.findViewById(R.id.Signin);
+        joinus = view.findViewById(R.id.signup);
+
+        navController = Navigation.findNavController(getActivity(), R.id.signin_host);
+
+        signin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Networkconfig networkCheck = new Networkconfig();
+
+                if (!checkEmptyFields()) {
+                    String a_email = admin_email.getText().toString();
+                    String a_pass = admin_pass.getText().toString();
+                    doSignin(a_email, a_pass);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            // For sending in drashboard and not login each time
+            updateUI(firebaseUser);
+            Toast.makeText(getActivity().getApplicationContext(), "User Already Signin.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void doSignin(String email, String pass)
+    {
+        firebaseAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(getActivity(), task -> {
+                     if (task.isSuccessful())
+                     {
+                        Toast.makeText(getActivity().getApplicationContext(), "Login Success!", Toast.LENGTH_SHORT).show();
+                        firebaseUser = firebaseAuth.getCurrentUser();
+                         updateUI(firebaseUser);
+                     } else {
+                        Toast.makeText(getActivity().getApplicationContext(), "Authenticate Failed!", Toast.LENGTH_SHORT).show();
+                     }
+                });
+    }
+
+    public void updateUI(FirebaseUser user)
+    {
+        Bundle b = new Bundle();
+        b.putParcelable("user",user);
+        navController.navigate(R.id.dashboard,b);
+    }
+
+    public boolean checkEmptyFields()
+    {
+        if(TextUtils.isEmpty(admin_email.getText().toString()))
+        {
+            admin_email.setError("Email cannot be empty!");
+            admin_email.requestFocus();
+            return true;
+        }else if (TextUtils.isEmpty(admin_pass.getText().toString()))
+        {
+            admin_pass.setError("Password cannot be empty!");
+            admin_pass.requestFocus();
+            return true;
+        }
+        return false;
     }
 }
