236a6eb4b23d45428f8ed9e99ba9592b23b1132a
Harsimran Saini <harsimran12326@gmail.com>
2021-06-24 02:04:53 -0400

    Fix UI inconsistencies for equipment list

--- app/src/main/java/com/cegep/sportify_admin/addEquipment/AddEquipmentActivity.java
+++ app/src/main/java/com/cegep/sportify_admin/addEquipment/AddEquipmentActivity.java
@@ -1,7 +1,9 @@
 package com.cegep.sportify_admin.addEquipment;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 import com.cegep.sportify_admin.R;
 
 public class AddEquipmentActivity extends AppCompatActivity {
@@ -10,5 +12,13 @@ public class AddEquipmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_equipment);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
