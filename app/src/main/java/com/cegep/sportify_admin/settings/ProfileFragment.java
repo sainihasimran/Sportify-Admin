6039f8a348dbe492663192ba8eb2c7266c52373e
Hemit Khunt <khunthemit1@gmail.com>
2021-06-26 23:01:08 -0400

    Log out

--- app/src/main/java/com/cegep/sportify_admin/settings/ProfileFragment.java
+++ app/src/main/java/com/cegep/sportify_admin/settings/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +17,8 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 
+import com.cegep.sportify_admin.LoginActivity;
+import com.cegep.sportify_admin.LoginFragment;
 import com.cegep.sportify_admin.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -75,6 +78,18 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+
+        Button button = view.findViewById(R.id.sign_out_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(requireActivity(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                requireActivity().finish();
             }
         });
 
